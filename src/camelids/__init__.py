a7e9e5f453af4704ee4308118f8836c28ced39ea
Alex Fedotov <alxfed@gmail.com>
2026-07-15 08:12:59 -0500

    utils

--- src/camelids/__init__.py
+++ src/camelids/__init__.py
@@ -5,12 +5,12 @@
 This source code is licensed under the license found in the
 LICENSE file in the root directory of this source tree.
 """
-from .rest import *
-from .adapter import *
+from .responses import respond
+from .chat import chat_complete
 
 
 __all__ = [
-    'continuation',
-    'decode',
-    'encode'
+    'chat_complete',
+    'respond',
+    'message'
 ]
